739c7783ea20d37b5f531d29347f0c21b24968d6
Marco Abate <marcoabate@teamitg.com>
2025-10-27 00:47:50 +0100

    splash screen and firebase packages

--- lyrica/App.tsx
+++ lyrica/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { PaperProvider } from "react-native-paper";
 import { Navigator } from "@/entities/routes";
 import { QueryProvider } from "@/entities/query";
@@ -7,14 +7,48 @@ import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.setOptions({
+  fade: true,
+});
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appReady, setAppReady] = useState(false);
+  const splashScreenShown = useRef(!appReady);
+
+  useEffect(() => {
+    const appSetup = async () => {
+      try {
+        // TODO: Load Firebase Configuration
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppReady(true);
+      }
+    };
+
+    appSetup();
+  }, []);
+
+  useEffect(() => {
+    if (appReady && splashScreenShown.current) {
+      SplashScreen.hide();
+      splashScreenShown.current = false;
+    }
+  }, [appReady]);
+
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <QueryProvider>
         <PaperProvider>
-          <StatusBar style="auto" />
-          <Navigator />
+          {appReady ? (
+            <>
+              <StatusBar style="auto" />
+              <Navigator />
+            </>
+          ) : null}
         </PaperProvider>
       </QueryProvider>
     </SafeAreaProvider>
---
+++ lyrica/src/entities/firebase/model/config.ts
@@ -0,0 +1,11 @@
+import { type ReactNativeFirebase } from "@react-native-firebase/app";
+
+export const credentials: ReactNativeFirebase.FirebaseAppOptions = {
+  clientId: "",
+  appId: "",
+  apiKey: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  projectId: "",
+};
